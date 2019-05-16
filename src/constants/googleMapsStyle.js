aed8cd96f51eb22c201a6533757cde99744aeb64
md-prog <progmad@outlook.com>
2019-05-16 06:07:02 +0200

    constants

---
+++ src/constants/googleMapsStyle.js
@@ -0,0 +1,274 @@
+export default [
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 20
+            },
+            {
+                "color": "#f5f5f5"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#a8a8a8"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#cccccc"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#d2d2d2"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#b9b9b9"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c2c1c1"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dbdbdb"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#d6d6d6"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e5e5e5"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#dcdbdb"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#c9c9c9"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#bdbaba"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#dfdfdf"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+];
+
+
+/** WEBPACK FOOTER **
+ ** ./constants/googleMapsStyle.js
+ **/
---
+++ src/constants/index.js
@@ -0,0 +1,98 @@
+// LANG
+export const LANG_IT = 'it-IT';
+export const LANG_DE = 'de-DE';
+
+//GLOBAL CONSTANTS
+export const API_BASE = 'http://admin.claraluna.it/api/v1/initialstate';
+export const API_STORES = 'http://admin.claraluna.it/api/v1/stores';
+export const IMAGES_BASE_URL =  'http://admin.claraluna.it';
+export const URL_ORIGIN = 'http://www.claraluna.it';
+export const DEFAULT_IMG = '/public/gallery/items/48/Clara_col.jpg';
+
+// Development constants
+export const _DEV_LOADER = true;
+export const _DEV_USE_DATABASE = false;
+
+// App
+export const MOBILE_WIDTH = 768;
+export const APP_IS_LOADED = 'APP_IS_LOADED';
+export const APP_SET_MOBILE = 'APP_SET_MOBILE';
+export const APP_UNSET_MOBILE = 'APP_UNSET_MOBILE';
+export const APP_SET_TABLET = 'APP_SET_TABLET';
+export const APP_UNSET_TABLET = 'APP_UNSET_TABLET';
+export const APP_SET_FETCHING = 'APP_SET_FETCHING';
+export const APP_UNSET_FETCHING = 'APP_UNSET_FETCHING';
+export const UPDATE_COLLECTION_OPEN_Y = 'UPDATE_COLLECTION_OPEN_Y';
+export const UPDATE_COLLECTION_HOVER_Y = 'UPDATE_COLLECTION_HOVER_Y';
+export const APP_SET_WINDOW_COORDINATES = 'APP_SET_WINDOW_COORDINATES';
+export const APP_SET_ACTIVE_CATEGORY = 'APP_SET_ACTIVE_CATEGORY';
+export const APP_UNSET_ACTIVE_CATEGORY = 'APP_UNSET_ACTIVE_CATEGORY';
+export const APP_SET_ACTIVE_COLLECTION = 'APP_SET_ACTIVE_COLLECTION';
+export const APP_UNSET_ACTIVE_COLLECTION = 'APP_UNSET_ACTIVE_COLLECTION';
+export const APP_SET_MOVING = 'APP_SET_MOVING';
+export const APP_UNSET_MOVING = 'APP_UNSET_MOVING';
+export const APP_SET_OPEN_COLLECTION = 'APP_SET_OPEN_COLLECTION';
+export const APP_UNSET_OPEN_COLLECTION = 'APP_UNSET_OPEN_COLLECTION';
+export const APP_SET_ACTIVE_PRODUCT = 'APP_SET_ACTIVE_PRODUCT';
+export const APP_UNSET_ACTIVE_PRODUCT = 'APP_UNSET_ACTIVE_PRODUCT';
+export const APP_SET_ZOOM_ACTIVE = 'APP_SET_ZOOM_ACTIVE';
+export const APP_UNSET_ZOOM_ACTIVE = 'APP_UNSET_ZOOM_ACTIVE';
+export const APP_SET_FRAME_VISIBLE = 'APP_SET_FRAME_VISIBLE';
+export const APP_UNSET_FRAME_VISIBLE = 'APP_UNSET_FRAME_VISIBLE';
+export const APP_BOOK_OPEN = 'APP_BOOK_OPEN';
+export const APP_BOOK_CLOSE = 'APP_BOOK_CLOSE';
+export const APP_BOOK_CLOSING = 'APP_BOOK_CLOSING';
+
+// Page loader
+export const PAGE_LOADER_SHOW = 'PAGE_LOADER_SHOW';
+export const PAGE_LOADER_HIDE = 'PAGE_LOADER_HIDE';
+
+// Frame Button
+export const FRAME_BUTTON_HIDE = 'FRAME_BUTTON_HIDE';
+export const FRAME_BUTTON_BOOK_AN_APPOINTMENT = 'FRAME_BUTTON_BOOK_AN_APPOINTMENT';
+export const FRAME_BUTTON_SEND = 'FRAME_BUTTON_SEND';
+export const FRAME_BUTTON_SORE_LOCATOR = 'FRAME_BUTTON_SORE_LOCATOR';
+
+// Logo
+export const LOGO_SHOW = 'LOGO_SHOW';
+export const LOGO_HIDE = 'LOGO_HIDE';
+
+// Vat number
+export const VAT_NUMBER_SHOW = 'VAT_NUMBER_SHOW';
+export const VAT_NUMBER_HIDE = 'VAT_NUMBER_HIDE';
+
+// Private area
+export const RESERVED_AREA_SHOW = 'RESERVED_AREA_SHOW';
+export const RESERVED_AREA_HIDE = 'RESERVED_AREA_HIDE';
+
+// Main Menu
+export const MAIN_MENU_SHOW_TRIGGER = 'MAIN_MENU_SHOW_TRIGGER';
+export const MAIN_MENU_HIDE_TRIGGER = 'MAIN_MENU_HIDE_TRIGGER';
+export const MAIN_MENU_OPEN = 'MAIN_MENU_OPEN';
+export const MAIN_MENU_CLOSING = 'MAIN_MENU_CLOSING';
+export const MAIN_MENU_CLOSE = 'MAIN_MENU_CLOSE';
+
+// View All Collection button
+export const VIEW_ALL_COLLECTION_HIDE = 'VIEW_ALL_COLLECTION_HIDE';
+export const VIEW_ALL_COLLECTION_SHOW = 'VIEW_ALL_COLLECTION_SHOW';
+export const VIEW_ALL_COLLECTION_PHONE = 'VIEW_ALL_COLLECTION_PHONE';
+
+// Products
+export const PRODUCT_UNSET_VISIBILITY = 'PRODUCT_UNSET_VISIBILITY';
+export const PRODUCT_SET_VISIBILITY = 'PRODUCT_SET_VISIBILITY';
+
+// Wishlist
+export const WISHLIST_ADD_ITEM = 'WISHLIST_ADD_ITEM';
+export const WISHLIST_REMOVE_ITEM = 'WISHLIST_REMOVE_ITEM';
+
+// Gallery
+export const GALLERY_SET_CURRENT = 'GALLERY_SET_CURRENT';
+
+// Cookie Law
+export const COOKIE_ACCEPT = 'COOKIE_ACCEPT';
+export const COOKIE_SHOW = 'COOKIE_SHOW';
+
+
+/** WEBPACK FOOTER **
+ ** ./constants/index.js
+ **/
